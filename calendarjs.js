9ba11aacf8f00e7c9d525ca9c405a21073687792
Mayank Aggarwal <mayank.aggarwal@cybergindia.local>
2021-04-19 12:44:55 +0530

    Calendar by Mayank Aggarwal

---
+++ calendarjs.js
@@ -0,0 +1,295 @@
+
+var x = document.getElementById("march");
+var y = document.getElementById("feb");
+var z = document.getElementById("jan");
+var a = document.getElementById("april");
+var b = document.getElementById("may");
+var c = document.getElementById("june");
+var d = document.getElementById("july");
+var e = document.getElementById("august");
+var f = document.getElementById("sep");
+var g = document.getElementById("oct");
+var h = document.getElementById("nov");
+var i = document.getElementById("dec");
+
+
+
+
+
+
+function myfunction(){
+
+    
+    
+    
+    x = document.getElementById("march");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+
+
+
+    } else {
+
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+           }
+  }
+  function myfunction2(){
+
+    
+
+    y = document.getElementById("feb");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+      z.style.display="none";
+      x.style.display="none";
+    } 
+    else {
+        y.style.display = "none";
+      z.style.display="none";
+      x.style.display="none";
+      }
+  }
+  function myfunction3(){
+
+
+    z = document.getElementById("jan");
+    if (z.style.display === "none") {
+      z.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        z.style.display = "none";
+      }
+  }
+  function myfunction4(){
+
+
+    a = document.getElementById("april");
+    if (a.style.display === "none") {
+      a.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        a.style.display = "none";
+      }
+  }
+  function myfunction5(){
+
+
+    b = document.getElementById("may");
+    if (b.style.display === "none") {
+      b.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        b.style.display = "none";
+      }
+  }
+  function myfunction6(){
+
+
+    c = document.getElementById("june");
+    if (c.style.display === "none") {
+      c.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        c.style.display = "none";
+      }
+  }
+  function myfunction7(){
+
+
+    d = document.getElementById("july");
+    if (d.style.display === "none") {
+      d.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        d.style.display = "none";
+      }
+  }
+  function myfunction8(){
+
+
+    e = document.getElementById("august");
+    if (e.style.display === "none") {
+      e.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        e.style.display = "none";
+      }
+  }
+  function myfunction9(){
+
+
+    f = document.getElementById("sep");
+    if (f.style.display === "none") {
+      f.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        f.style.display = "none";
+      }
+  }
+  function myfunction10(){
+
+
+    g = document.getElementById("oct");
+    if (g.style.display === "none") {
+      g.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      h.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        g.style.display = "none";
+      }
+  }
+  function myfunction11(){
+
+
+    h = document.getElementById("nov");
+    if (h.style.display === "none") {
+      h.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      i.style.display="none";
+    } 
+    else {
+        h.style.display = "none";
+      }
+  }
+  function myfunction12(){
+
+
+    i = document.getElementById("dec");
+    if (i.style.display === "none") {
+      i.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display="none";
+      a.style.display="none";
+      b.style.display="none";
+      c.style.display="none";
+      d.style.display="none";
+      e.style.display="none";
+      f.style.display="none";
+      g.style.display="none";
+      h.style.display="none";
+    } 
+    else {
+        i.style.display = "none";
+      }
+  }
+
+
